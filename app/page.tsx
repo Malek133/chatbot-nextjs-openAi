f6c015c83c69632d935aa45be5a9cbe843e5faa7
unknown <amazighmalek.com>
2024-05-07 03:46:47 +0200

    code

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,224 @@
-import Image from "next/image";
+// 'use client'
+
+// import { TextArea } from "@/src/components/TextArea";
+// import { FormEvent, useRef, useState } from "react";
+//  import { ChatCompletionRequestMessage,
+//   Configuration,OpenAI } from 'openai';
+// import { Message } from "@/src/components/Message";
+// // import { Loader } from "@/src/components/Loader";
+
+// const createChatCompletion = (messages: 
+//   ChatCompletionRequestMessage[]) => {
+//   const configuration = new Configuration({
+//     apiKey: process.env.NEXT_PUBLIC_OPENAI_KEY,
+//   });
+//   const openai = new OpenAI(configuration);
+
+//   return openai.createChatCompletion({
+//     model: 'gpt-3.5-turbo',
+//     messages: messages,
+//   });
+
+// }
+
+// export default function Home() {
+//   const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([]);
+//   const ref = useRef<HTMLUListElement>(null);
+
+//   const handleSubmit = (e:FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//     const formData = new FormData(e.currentTarget);
+
+//     const user = String(formData.get('user'));
+//     const newMessage = {
+//       role: 'user',
+//       content: user,
+//     } satisfies ChatCompletionRequestMessage;
+//     const newMessages = [...messages, newMessage];
+//     setMessages(newMessages);
+//     scrollToLastMessage();
+//     createChatCompletion(newMessages).then((response) =>{
+//       const newText = response.data.data.choices[0].message?.content;
+//       if(!newText){
+//         return;
+//       }
+//       setMessages((prevMessage) =>[
+//         ...prevMessage,
+//         {
+//           role:'assistant',
+//           content: newText
+//         }
+//       ])
+//       scrollToLastMessage();
+//     })
+    
+//     e.currentTarget.reset();
+
+//   }
+
+//   const scrollToLastMessage = () => {
+//     setTimeout(() => {
+//       ref.current?.children[ref.current?.children.length - 1].scrollIntoView();
+//     }, 1);
+//   };
+
+//   return (
+//     <main className="m-auto max-w-xl flex flex-col px-2 py-8 h-full">
+//       <div className="flex-1 flex flex-col gap-4 overflow-auto">
+//         <h1 className="text-2xl md:text-4xl font-bold text-center">ChatGPTA0</h1>
+//         <ul ref={ref} className="flex flex-col flex-1">
+//           {messages.map((message, i) => (
+//             <Message message={message} key={message.content + i} />
+//           ))}
+//           {messages.length === 0 && <li>
+//             No messages yet, start a conversation!</li>}
+
+//           {/* {mutation.isLoading && ( */}
+//             {/* <li className="flex items-center w-full p-4">
+//               <Loader />
+//               <p className="text-zinc-300 animate-pulse">
+//                 ChatGEPETO is thinking...
+//               </p>
+//             </li> */}
+//            {/* )} */}
+//         </ul>
+//         </div>
+//         <form onSubmit={handleSubmit}>
+//         <fieldset className="flex items-end gap-2">
+//           <div className="flex-1 my-2">
+//           <TextArea name="user" label="Your message" />
+//           </div>
+
+//           <button type="submit"
+//       className="text-white disabled:dark:bg-blue-900
+//        disabled:dark:text-gray-400 disabled:text-gray-400
+//         disabled:bg-blue-500 disabled:cursor-not-allowed
+//          bg-blue-800 hover:bg-blue-900 focus:ring-4
+//           focus:ring-blue-400 font-medium rounded-lg 
+//           text-sm px-5 py-2.5  dark:bg-blue-700
+//            dark:hover:bg-blue-800 focus:outline-none
+//             dark:focus:ring-blue-900">
+//              Submit</button>
+
+//         </fieldset>
+//         </form>
+//     </main>
+//   );
+// }
+
+'use client';
+
+import { Loader } from '@/src/components/Loader';
+import { Message } from '@/src/components/Message';
+import { TextArea } from '@/src/components/TextArea';
+import { useMutation } from '@tanstack/react-query';
+import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from 'openai';
+import { FormEvent, useRef, useState } from 'react';
+
+
+const createChatCompletion = (messages: ChatCompletionRequestMessage[]) => {
+  const configuration = new Configuration({
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_KEY,
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  return openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: messages,
+  });
+};
 
 export default function Home() {
+  const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([]);
+
+  const ref = useRef<HTMLUListElement>(null);
+
+  const mutation = useMutation(
+    (newMessages: ChatCompletionRequestMessage[]) =>
+      createChatCompletion(newMessages),
+    {
+      onSuccess: (response) => {
+        const newText = response.data.choices[0].message?.content;
+
+        if (!newText) {
+          return;
+        }
+
+        setMessages((prevMessages) => [
+          ...prevMessages,
+          {
+            role: 'assistant',
+            content: newText,
+          },
+        ]);
+
+        scrollToLastMessage();
+      },
+    }
+  );
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    const user = String(formData.get('user'));
+
+    const newMessage = {
+      role: 'user',
+      content: user,
+    } satisfies ChatCompletionRequestMessage;
+
+    const newMessages = [...messages, newMessage];
+
+    setMessages(newMessages);
+    scrollToLastMessage();
+
+    e.currentTarget.reset();
+
+    mutation.mutate(newMessages);
+  };
+
+  const scrollToLastMessage = () => {
+    setTimeout(() => {
+      ref.current?.children[ref.current?.children.length - 1].scrollIntoView();
+    }, 1);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="m-auto max-w-xl flex flex-col px-2 py-8 h-full">
+      <div className="flex-1 flex flex-col gap-4 overflow-auto">
+        <h1 className="text-3xl md:text-5xl font-bold text-center">ChatGEPETO</h1>
+        <ul ref={ref} className="flex flex-col flex-1">
+          {messages.map((message, i) => (
+            <Message message={message} key={message.content + i} />
+          ))}
+          {messages.length === 0 && <li>No messages yet, start a conversation!</li>}
+
+          {mutation.isLoading && (
+            <li className="flex items-center w-full p-4">
+              <Loader />
+              <p className="text-zinc-300 animate-pulse">
+                ChatGEPETO is thinking...
+              </p>
+            </li>
+          )}
+        </ul>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <fieldset disabled={mutation.isLoading} className="flex items-end gap-2">
+          <div className="flex-1">
+            <TextArea name="user" label="Your message" />
+          </div>
+          <button
+            type="submit"
+            className="text-white disabled:dark:bg-green-800 disabled:dark:text-zinc-400 disabled:text-zinc-400 disabled:bg-green-300 disabled:cursor-not-allowed bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5  dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+            Submit
+          </button>
+        </fieldset>
+      </form>
     </main>
   );
 }
